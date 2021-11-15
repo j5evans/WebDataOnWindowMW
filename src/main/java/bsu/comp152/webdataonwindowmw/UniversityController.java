6e9f342d5c92aa3f56a44c24f4f4eab82d6ee187
j5evans <j5evans@student.bridgew.edu>
2021-11-15 15:02:59 -0500

    commit

--- src/main/java/bsu/comp152/webdataonwindowmw/UniversityController.java
+++ src/main/java/bsu/comp152/webdataonwindowmw/UniversityController.java
@@ -1,14 +1,57 @@
 package bsu.comp152.webdataonwindowmw;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 
-public class UniversityController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UniversityController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField NameField;
+
+    @FXML
+    private TextField UniversityCountry;
+
+    @FXML
+    private TextField websiteDisplayField;
+
+    @FXML
+    private ListView<DataHandler.UniversityDataType> ListControl;
+
+    private DataHandler Model;
+
+    public void loadData() {
+        var site = "http://universities.hipolabs.com/search?name=";
+        var params = getQueryParams();
+        var query = site + params;
+        Model = new DataHandler(query);
+        var univList = Model.getData();
+        ObservableList<DataHandler.UniversityDataType> dataToShow = FXCollections.observableArrayList(univList);
+        ListControl.setItems(dataToShow);
+    }
+
+    private String getQueryParams() {
+        TextInputDialog inputGrabber = new TextInputDialog("Young");
+        inputGrabber.setHeaderText("Gathering Information for Query");
+        inputGrabber.setContentText("Enter University Name:");
+        var name = inputGrabber.showAndWait();
+        if (name.isPresent()) {
+            return name.get();
+        }
+        else {
+            return "";
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadData();
     }
 }
